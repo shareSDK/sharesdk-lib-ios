5c7c545835b1292a591cfb51a7f540c725a85015
jessecurry <jesse@jessecurry.net>
2012-12-16 17:03:39 -0500

    Initial Commit

---
+++ sharesdk-lib/sharesdk_lib.h
@@ -0,0 +1,13 @@
+//
+//  sharesdk_lib.h
+//  sharesdk-lib
+//
+//  Created by Jesse Curry on 12/16/12.
+//  Copyright (c) 2012 ShareSDK. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface sharesdk_lib : NSObject
+
+@end
---
+++ sharesdk-lib/sharesdk_lib.m
@@ -0,0 +1,13 @@
+//
+//  sharesdk_lib.m
+//  sharesdk-lib
+//
+//  Created by Jesse Curry on 12/16/12.
+//  Copyright (c) 2012 ShareSDK. All rights reserved.
+//
+
+#import "sharesdk_lib.h"
+
+@implementation sharesdk_lib
+
+@end
